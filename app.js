c590de299e6b436b7aa31f71c7ebf706987d3b71
Dom <domborgesbr2468@gmail.com>
2023-11-08 18:02:39 -0300

    feat: color background

---
+++ app.js
@@ -0,0 +1,5 @@
+const button = document.querySelector(".bnt-cor");
+const body = document.querySelector("body");
+button.addEventListener("click", function changerColor() {
+  body.style.backgroundColor = "#940B92";
+});
